e515768e45eda8c759aa838d8b8830c6268a769a
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 14:48:10 +0800

    [FizzBuss] [Jeyem] Refactor: Created method for Modulo and declare Constant variable for repeatable value

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,30 +5,38 @@ public class FizzBuzz {
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
     public static final String WHIZZ = "Whizz";
+    public static final int ZERO = 0;
+    public static final int THREE = 3;
+    public static final int FIVE = 5;
+    public static final int SEVEN = 7;
 
     public String getResult(int orderNumber){
-        if(orderNumber%3 == 0 && orderNumber%5 == 0 && orderNumber%7 == 0){
+        if(isModulo(orderNumber, THREE) && isModulo(orderNumber, FIVE) && isModulo(orderNumber, SEVEN)){
             return new StringBuilder().append(FIZZ).append(BUZZ).append(WHIZZ).toString();
         }
-        else if(orderNumber%3 == 0 && orderNumber%5 == 0){
+        if(isModulo(orderNumber, THREE) && isModulo(orderNumber, FIVE)){
             return new StringBuilder().append(FIZZ).append(BUZZ).toString();
         }
-        else if(orderNumber%3 == 0 && orderNumber%7 == 0){
+        if(isModulo(orderNumber, THREE) && isModulo(orderNumber, SEVEN)){
             return new StringBuilder().append(FIZZ).append(WHIZZ).toString();
         }
-        else if(orderNumber%5 == 0 && orderNumber%7 == 0){
+        if(isModulo(orderNumber, FIVE) && isModulo(orderNumber, SEVEN)){
             return new StringBuilder().append(BUZZ).append(WHIZZ).toString();
         }
-        else if(orderNumber%3 == 0){
+        if(isModulo(orderNumber, THREE)){
             return FIZZ;
         }
-        else if(orderNumber%5 == 0){
+        if(isModulo(orderNumber, FIVE)){
             return BUZZ;
         }
-        else if(orderNumber%7 == 0){
+        if(isModulo(orderNumber, 7)){
             return WHIZZ;
-        }else{
-            return String.valueOf(orderNumber);
         }
+        return String.valueOf(orderNumber);
     }
+
+    private boolean isModulo(int orderNumber, int modulo) {
+        return orderNumber % modulo == ZERO;
+    }
+
 }
