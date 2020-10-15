098089e9661ce921b665ea45b1aca49fe3298da7
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 14:10:08 +0800

    [FizzBuss] [Jeyem] Feature: Added Buzz result for numbers can be multiplied by 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,14 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
 
     public String getResult(int orderNumber){
         if(orderNumber%3 == 0){
             return FIZZ;
+        }
+        else if(orderNumber%5 == 0){
+            return BUZZ;
         }
         return null;
     }
