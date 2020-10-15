6e35861cdefebe931f9ed9f4152c9e7b98907cc1
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 14:14:41 +0800

    [FizzBuss] [Jeyem] Feature: Added Whizz result for numbers can be multiplied by 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
 
     public String getResult(int orderNumber){
         if(orderNumber%3 == 0){
@@ -11,6 +12,9 @@ public class FizzBuzz {
         }
         else if(orderNumber%5 == 0){
             return BUZZ;
+        }
+        else if(orderNumber%7 == 0){
+            return WHIZZ;
         }
         return null;
     }
