9c2472cdc5e99ca22d26ed9173a4f8c97abddba0
PONCEJE <PONCEJE@OOCL.COM>
2020-10-15 14:03:26 +0800

    [FizzBuss] [Jeyem] Test: should_return_Fizz_when_count_off_given_order_number

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public String getResult(int orderNumber){
+
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,22 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_Fizz_when_count_off_given_order_number(){
+        //
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int orderNumber = 3;
+        //
+        String result = fizzBuzz.getResult(orderNumber);
+        //
+        assertEquals(result,"Fizz");
+
+    }
+
+
 }
